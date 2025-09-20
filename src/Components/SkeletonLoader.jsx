1b56fb90f76e79dcdfa21e96a2b6920ba37313d1
chiemezue <113723263+chiemezue@users.noreply.github.com>
2025-09-20 12:59:06 +0100

    nice

--- src/Components/SkeletonLoader.jsx
+++ src/Components/SkeletonLoader.jsx
@@ -6,20 +6,21 @@ const SkeletonLoader = ({ count = 4 }) => {
       {Array.from({ length: count }).map((_, index) => (
         <div
           key={index}
-          className="flex flex-col md:flex-row  rounded-lg overflow-hidden animate-pulse"
+          className="flex flex-col rounded-lg overflow-hidden animate-pulse bg-gray-800"
         >
           {/* Image placeholder */}
-          <div className="md:w-1/3 h-40 bg-gray-700"></div>
+          <div className="w-full h-48 bg-gray-700"></div>
+
           {/* Content placeholder */}
-          <div className="flex-1 p-4 flex flex-col justify-between gap-2">
-            <div className="h-4 bg-gray-700 rounded w-1/2"></div>{" "}
+          <div className="p-4 flex flex-col gap-2">
+            <div className="h-4 bg-gray-600 rounded w-1/3"></div>{" "}
             {/* date & read time */}
-            <div className="h-6 bg-gray-700 rounded w-3/4"></div> {/* title */}
-            <div className="h-4 bg-gray-700 rounded w-full"></div>{" "}
+            <div className="h-6 bg-gray-600 rounded w-3/4"></div> {/* title */}
+            <div className="h-4 bg-gray-600 rounded w-full"></div>{" "}
             {/* subtitle line */}
-            <div className="h-4 bg-gray-700 rounded w-5/6"></div>{" "}
+            <div className="h-4 bg-gray-600 rounded w-5/6"></div>{" "}
             {/* subtitle line */}
-            <div className="h-4 bg-gray-700 rounded w-1/4 mt-2"></div>{" "}
+            <div className="h-4 bg-gray-600 rounded w-1/4 mt-2"></div>{" "}
             {/* comments */}
           </div>
         </div>
