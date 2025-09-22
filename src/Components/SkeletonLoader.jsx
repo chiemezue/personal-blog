b7167beab0f28d0e41525116fd972d52c361feb8
chiemezue <113723263+chiemezue@users.noreply.github.com>
2025-09-22 15:09:23 +0100

    new

--- src/Components/SkeletonLoader.jsx
+++ src/Components/SkeletonLoader.jsx
@@ -9,18 +9,18 @@ const SkeletonLoader = ({ count = 4 }) => {
           className="flex flex-col rounded-lg overflow-hidden animate-pulse bg-gray-400"
         >
           {/* Image placeholder */}
-          <div className="w-full h-48 bg-gray-700"></div>
+          <div className="w-full h-48 bg-gray-400"></div>
 
           {/* Content placeholder */}
           <div className="p-4 flex flex-col gap-2">
-            <div className="h-4 bg-gray-600 rounded w-1/3"></div>{" "}
+            <div className="h-4 bg-gray-400 rounded w-1/3"></div>{" "}
             {/* date & read time */}
-            <div className="h-6 bg-gray-600 rounded w-3/4"></div> {/* title */}
-            <div className="h-4 bg-gray-600 rounded w-full"></div>{" "}
+            <div className="h-6 bg-gray-400 rounded w-3/4"></div> {/* title */}
+            <div className="h-4 bg-gray-400 rounded w-full"></div>{" "}
             {/* subtitle line */}
-            <div className="h-4 bg-gray-600 rounded w-5/6"></div>{" "}
+            <div className="h-4 bg-gray-400 rounded w-5/6"></div>{" "}
             {/* subtitle line */}
-            <div className="h-4 bg-gray-600 rounded w-1/4 mt-2"></div>{" "}
+            <div className="h-4 bg-gray-400 rounded w-1/4 mt-2"></div>{" "}
             {/* comments */}
           </div>
         </div>
